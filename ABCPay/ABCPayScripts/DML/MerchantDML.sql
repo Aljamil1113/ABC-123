8b7dd11eb6d629bdef97d1137a39fba845426d9c
Aljamil1113 <aarazasaljamil@gmail.com>
2021-03-31 20:30:50 +0800

    Seed Some Data

---
+++ ABCPay/ABCPayScripts/DML/MerchantDML.sql
@@ -0,0 +1,29 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+IF NOT EXISTS(Select * from Merchants where MerchantName = 'AT&T')
+BEGIN
+   INSERT INTO Merchants(MerchantName) VALUES('AT&T');
+END
+GO
+
+IF NOT EXISTS(Select * from Merchants where MerchantName = 'TELEPAY')
+BEGIN
+   INSERT INTO Merchants(MerchantName) VALUES('TELEPAY');
+END
+GO
+
+IF NOT EXISTS(Select * from Merchants where MerchantName = 'Netflix')
+BEGIN
+   INSERT INTO Merchants(MerchantName) VALUES('Netflix');
+END
+GO
---
+++ Pay123/Pay123SqlScripts/DML/StatusDML.sql
@@ -0,0 +1,36 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+IF NOT EXISTS(Select * from Statuses where StatusName = 'Pending')
+BEGIN
+   INSERT INTO Statuses(StatusName) VALUES('Pending');
+END
+GO
+
+IF NOT EXISTS(Select * from Statuses where StatusName = 'Processing')
+BEGIN
+   INSERT INTO Statuses(StatusName) VALUES('Processing');
+END
+GO
+
+IF NOT EXISTS(Select * from Statuses where StatusName = 'Done')
+BEGIN
+   INSERT INTO Statuses(StatusName) VALUES('Done');
+END
+GO
+
+IF NOT EXISTS(Select * from Statuses where StatusName = 'Failed')
+BEGIN
+   INSERT INTO Statuses(StatusName) VALUES('Failed');
+END
+GO
